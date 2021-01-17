d456383c079df13d8ff29ae05f3c9057344c1e43
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-01-17 17:15:12 +0530

    added auto mouse mover

---
+++ python_programs/auto_mouse_mover.py
@@ -0,0 +1,12 @@
+import time
+import pyautogui
+
+print('Auto Mouse Mover Activated, press Ctrl-C to quit.')
+
+try:
+    while True:
+        pyautogui.moveRel(10, 0, 0.5)
+        pyautogui.moveRel(-10, 0, 0.5)
+        time.sleep(10)
+except KeyboardInterrupt:
+    print('Auto Mouse Mover De-Activated.')
--- python_programs/kmph_to_mph.py
+++ python_programs/kmph_to_mph.py
@@ -1,3 +1,3 @@
 kmph = int(input('Enter km/h : '))
 mph = 0.621371 * kmph
-print(kmph,'km/h =',mph,'m/h')
+print(kmph, 'km/h =', mph, 'm/h')
