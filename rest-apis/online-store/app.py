f8b97fe701cb68fd989426e8c59514559535ccb2
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-08-24 00:18:23 +0530

    Add implementation for CRUD operations

--- rest-apis/online-store/app.py
+++ rest-apis/online-store/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -21,9 +21,16 @@ stores = [
 ]
 
 
+# POST /store  {name:x}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 @app.route('/store')
@@ -32,18 +39,36 @@ def get_stores():
 
 
 @app.route('/store/<string:name>')
-def get_store():
-    pass
+def get_store(name):
+    # iterate over stores
+    # if the store name matches, return it
+    # otherwise, return an error message
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
 
 
 @app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store():
-    pass
+def create_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            request_data = request.get_json()
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+        store['items'].append(new_item)
+        return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
 
 @app.route('/store/<string:name>/item')
-def get_items_from_store():
-    pass
+def get_items_from_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 
 app.run(port=8090)
