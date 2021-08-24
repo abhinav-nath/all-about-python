5faec5513fc1b3fee52de577768765d5df3eaf81
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-08-25 00:39:34 +0530

    Add functionality to delete an item from a store

--- rest-apis/online-store/app.py
+++ rest-apis/online-store/app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 # list of dictionaries
 stores = [
     {
-        'name': 'Toy Store',
+        'store_name': 'Toy Store',
         'items': [
             {
-                'name': 'Rock Crawler',
+                'item_name': 'Rock Crawler',
                 'price': 50.45
             },
             {
-                'name': 'Ironman Action Figure',
+                'item_name': 'Ironman Action Figure',
                 'price': 30.25
             }
         ]
@@ -21,7 +21,7 @@ stores = [
 ]
 
 
-# POST /store  {name:x}
+# POST /store  {store_name:x}
 @app.route('/store', methods=['POST'])
 def create_store():
     request_data = request.get_json()
@@ -38,36 +38,48 @@ def get_stores():
     return jsonify({'stores': stores})
 
 
-@app.route('/store/<string:name>')
-def get_store(name):
+@app.route('/store/<string:store_name>')
+def get_store(store_name):
     # iterate over stores
     # if the store name matches, return it
     # otherwise, return an error message
     for store in stores:
-        if store['name'] == name:
+        if store['store_name'] == store_name:
             return jsonify(store)
     return jsonify({'message': 'store not found'})
 
 
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
+@app.route('/store/<string:store_name>/item', methods=['POST'])
+def create_item_in_store(store_name):
     for store in stores:
-        if store['name'] == name:
+        if store['store_name'] == store_name:
             request_data = request.get_json()
             new_item = {
-                'name': request_data['name'],
+                'item_name': request_data['item_name'],
                 'price': request_data['price']
             }
-        store['items'].append(new_item)
-        return jsonify(new_item)
+            store['items'].append(new_item)
+            return jsonify(new_item)
     return jsonify({'message': 'store not found'})
 
 
-@app.route('/store/<string:name>/item')
-def get_items_from_store(name):
+@app.route('/store/<string:store_name>/item')
+def get_items_from_store(store_name):
     for store in stores:
-        if store['name'] == name:
+        if store['store_name'] == store_name:
             return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
+
+
+@app.route('/store/<string:store_name>/item/<string:item_name>', methods=['DELETE'])
+def delete_item_from_store(store_name, item_name):
+    for store in stores:
+        if store['store_name'] == store_name:
+            for item in store['items']:
+                if item['item_name'] == item_name:
+                    store['items'].remove(item)
+                    return item_name + ' deleted successfully'
+                return item_name + ' not found'
     return jsonify({'message': 'store not found'})
 
 
