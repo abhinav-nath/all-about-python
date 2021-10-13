4872daf5941ef14c0b8976a497c0714615e10886
Abhinav Nath <abhinavnath@ymail.com>
2021-10-13 23:32:17 +0530

    Update lambda_functions.py

--- python_programs/basic/lambda_functions.py
+++ python_programs/basic/lambda_functions.py
@@ -1,11 +1,23 @@
+# Lambda - one liner anonymous function
+
 def add(a, b):
     return a + b
 
 
+# not a correct way of writing lambda
+# if we are binding it with a name then
+# better define a regular function
 minus = lambda a, b: a - b
 
 print("add", add(2, 5))
-print("minus", minus(8, 3))  # one liner anonymous function
+print("minus", minus(8, 3))
 
+# correct way to execute a lambda
+print("minus", (lambda a, b: a - b)(8, 3))
+
+# incorrect
 full_name = lambda fn, ln: fn.strip().title() + " " + ln.strip().title()
 print(full_name("    rick", "gRimes   "))
+
+# correct way
+print((lambda fn, ln: fn.strip().title() + " " + ln.strip().title())("    rick", "gRimes   "))
