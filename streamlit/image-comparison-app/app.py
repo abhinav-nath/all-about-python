d6c8883e76953be568f36c95989d69dcbc0a7da0
Abhinav Nath <abhinavnath@ymail.com>
2023-01-29 13:19:15 +0530

    Add an image comparison streamlit app

---
+++ streamlit/image-comparison-app/app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from streamlit_image_comparison import image_comparison
+
+
+st.set_page_config("Webb Space Telescope vs Hubble Telescope", "🔭")
+
+
+st.image(
+    "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/telescope_1f52d.png",
+    width=120,
+)
+
+st.header("J. Webb Space Telescope vs Hubble Telescope")
+
+st.write("")
+"This is a reproduction of the fantastic [WebbCompare](https://www.webbcompare.com/index.html) app by [John Christensen](https://twitter.com/JohnnyC1423). It's built in Streamlit and takes only 10 lines of Python code. If you like this app, please star [John's original repo](https://github.com/JohnEdChristensen/WebbCompare)!"
+st.write("")
+
+st.markdown("### Southern Nebula")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg",
+    img2="https://www.webbcompare.com/img/webb/southern_nebula_700.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
+
+
+st.markdown("### Galaxy Cluster SMACS 0723")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
+    img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
+
+st.markdown("### Carina Nebula")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/carina_2800.png",
+    img2="https://www.webbcompare.com/img/webb/carina_2800.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
+
+st.markdown("### Stephan's Quintet")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/stephans_quintet_2800.jpg",
+    img2="https://www.webbcompare.com/img/webb/stephans_quintet_2800.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
