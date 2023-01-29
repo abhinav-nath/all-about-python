340494631d1f8c7f9d946983744a07f3dd1153ff
Abhinav Nath <abhinavnath@ymail.com>
2023-01-29 12:25:42 +0530

    Add more Streamlit functions

--- streamlit/streamlit_basics.py
+++ streamlit/streamlit_basics.py
@@ -29,7 +29,7 @@ st.error("Error")
 
 # Exception - This has been added later
 exp = ZeroDivisionError("Trying to divide by Zero")
-st.execption(exp)
+st.exception(exp)
 
 # Write text
 st.write("Text with write")
@@ -38,6 +38,8 @@ st.write("Text with write")
 # This is not possible using st.text().
 st.write(range(10))
 
+
+# Check box
 # A checkbox returns a boolean value. When the box is checked, it returns a True value else returns a False value.
 # check if the checkbox is checked
 # title of the checkbox is 'Show/Hide'
@@ -46,7 +48,7 @@ if st.checkbox("Show/Hide"):
     st.text("Showing the widget")
 
 
-# radio button
+# Radio button
 # first argument is the title of the radio button
 # second argument is the options for the radio button
 status = st.radio("Select Gender: ", ('Male', 'Female'))
@@ -59,3 +61,57 @@ if (status == 'Male'):
 else:
     st.success("Female")
 
+
+# Selection box
+# first argument takes the title of the selection box
+# second argument takes options
+hobby = st.selectbox("Hobbies: ",
+                     ['Dancing', 'Reading', 'Sports'])
+
+# print the selected hobby
+st.write("Your hobby is: ", hobby)
+
+
+# Multi-Selectbox
+# first argument takes the box title
+# second argument takes the options to show
+hobbies = st.multiselect("Hobbies: ",
+                         ['Dancing', 'Reading', 'Sports'])
+
+# write the selected options
+st.write("You selected", len(hobbies), 'hobbies')
+
+
+# Button
+# Create a simple button that does nothing
+st.button("Click me for no reason")
+
+# Create a button, that when clicked, shows a text
+if(st.button("Click me!")):
+    st.text("Button clicked!!!")
+
+
+# Text Input
+
+# save the input text in the variable 'name'
+# first argument shows the title of the text input box
+# second argument displays a default text inside the text input area
+name = st.text_input("Enter Your name", "Type Here ...")
+
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+    result = name.title()
+    st.success(result)
+
+
+# Slider
+# first argument takes the title of the slider
+# second argument takes the starting of the slider
+# last argument takes the end number
+level = st.slider("Select the level", 1, 5)
+
+# print the level
+# format() is used to print value
+# of a variable at a specific position
+st.text('Selected: {}'.format(level))
